4656cb07677d153763b54f5f91e4ed11a7eb646a
rukiya <rukiyanusha123@gmail.com>
2026-06-30 09:40:15 +0530

    add js basics

--- sample.js
+++ sample.js
@@ -16,4 +16,15 @@ let a=10;
 let b=20;
 
 console.log(a+b)
+//////////////////////////////////////////////////////////////////////////////
+let name="Aksa";
+let age=19;
+let isstudent=true;
+let mark;
+let phone=null;
 
+console.log(name);
+console.log(age);
+console.log(isstudent);
+console.log(mark);
+console.log(phone);
