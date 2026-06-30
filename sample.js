73ed262ac587890e182a9b0d79a2b3638b0f8042
rukiya <rukiyanusha123@gmail.com>
2026-06-30 09:28:09 +0530

    add js basics

---
+++ sample.js
@@ -0,0 +1,19 @@
+let studentname ="Nus-Ha";
+let age =19;
+let course ="Mern stack";
+let city ="Ernakulam"
+
+console.log(studentname);
+console.log(age);
+console.log(course);
+console.log(city);
+
+
+
+///////////////////////////////////////////////////////////////////////////
+
+let a=10;
+let b=20;
+
+console.log(a+b)
+
